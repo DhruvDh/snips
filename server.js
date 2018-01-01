3d5a85839f818a50fc38b98e60904c43dd619053
DhruvDh <dhruv.dhamani@gmail.com>
2018-01-01 17:25:51 +0530

    code cleanup

--- server.js
+++ server.js
@@ -7,35 +7,103 @@ var fs = require('fs');
 var natural = require('natural');
 var classifier = new natural.BayesClassifier();
 
-var intents = ['AddToPlaylist', 'BookRestaurant', 'GetWeather', 'PlayMusic', 'RateBook', 'SearchCreativeWork', 'SearchScreeningEvent'];
+var intents = {
+    list: ['AddToPlaylist', 'BookRestaurant', 'GetWeather', 'PlayMusic', 'RateBook', 'SearchCreativeWork', 'SearchScreeningEvent'], 
+    'AddToPlaylist': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    'BookRestaurant': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    'GetWeather': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    'PlayMusic': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    'RateBook': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    'SearchCreativeWork': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    'SearchScreeningEvent': {
+        truePositive: 0,
+        total: 0,
+        falsePositive: 0,
+        falseNegative: 0
+    },
+    totalTruePositive: 0,
+    totalFalsePositive: 0,
+    totalFalseNegative: 0,
+    total: 0,
+    accuracy: 0.0
+}
 
-for(var i = 0; i < intents.length; i++) {
-    var docs = JSON.parse(fs.readFileSync(path.join(__dirname, 'cleaner', 'cleaned', 'train_'+intents[i]+'.clean.json')));
+for(var i = 0; i < intents.list.length; i++) {
+    var docs = JSON.parse(fs.readFileSync(path.join(__dirname, 'cleaner', 'cleaned', 'train_'+intents.list[i]+'.clean.json')));
     for(var j = 0; j < docs.length; j++) {
-        classifier.addDocument(docs[j], intents[i]);
+        classifier.addDocument(docs[j], intents.list[i]);
     }
 }
 
 classifier.train();
 
-console.log(classifier.classify("will it rain in mumbai"));
 
 fs.writeFile(path.join(__dirname, "classifiers", "trainedNaiveBayes.json"), JSON.stringify(classifier), 'utf8', function(err){
     if (err) console.log(err);
 });
 
-for(var i = 0; i < intents.length; i++) {
-    var docs = JSON.parse(fs.readFileSync(path.join(__dirname, 'cleaner', 'cleaned', 'train_'+intents[i]+'_full.clean.json')));
-    var positive = 0;
+for(var i = 0; i < intents.list.length; i++) {
+    var current = intents.list[i];
+    var docs = JSON.parse(fs.readFileSync(path.join(__dirname, 'cleaner', 'cleaned', 'train_'+current+'_full.clean.json')));
+    intents[current].total = docs.length + 1;
     for(var j = 0; j < docs.length; j++) {
-        if(classifier.classify(docs[j]) == intents[i])
-            positive++;
+        var Class = classifier.classify(docs[j]); 
+        if(Class == current)
+            intents[current].truePositive++;
         else {
-            //console.log("For '"+docs[j]+"' --> "+classifier.classify(docs[j])+" || Expected: "+intents[i]);
+            intents[Class].falsePositive++;
+            intents[current].falseNegative++;
         }
     }
-    console.log("For "+intents[i]+"'s classification: "+((positive/docs.length)*100.0));
+    console.log("For "+current+"'s classification: "+((intents[current].truePositive/intents[intents.list[i]].total)*100.0));
 }
+
+intents.list.forEach(function(intent) {
+    intents.totalTruePositive += intents[intent].truePositive;
+    intents.totalFalsePositive  += intents[intent].falsePositive;
+    intents.totalFalseNegative  += intents[intent].falseNegative;
+    intents.total += intents[intent].total;
+});
+
+fs.writeFile(path.join(__dirname, 'log', 'firstNaiveBayesAttempt.json'), JSON.stringify(intents), 'utf8', function(err) {
+    if (err) console.log(err);
+});
+
+intents.accuracy = (intents.totalTruePositive/intents.total)*100.0;
+console.log("Total Accuracy: "+intents.accuracy);
+
+
+
 
 app.use(bodyParser.urlencoded({
     extended: true
